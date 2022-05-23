4d7c9e8849b33ee51530527818aee9e7fe7460fb
Giorgio Azzinnaro <giorgio@azzinna.ro>
2022-05-23 15:23:14 +0200

    init

---
+++ lib/ex_twelvedata.ex
@@ -0,0 +1,18 @@
+defmodule ExTwelvedata do
+  @moduledoc """
+  Documentation for `ExTwelvedata`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTwelvedata.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_twelvedata_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTwelvedataTest do
+  use ExUnit.Case
+  doctest ExTwelvedata
+
+  test "greets the world" do
+    assert ExTwelvedata.hello() == :world
+  end
+end
