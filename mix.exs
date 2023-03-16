17d2142018c483b76dabb6454e2bf06ab47ff1bc
Giorgio Azzinnaro <giorgio@azzinna.ro>
2023-03-16 20:23:17 +0100

    chore: release 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTwelveData.MixProject do
   def project do
     [
       app: :ex_twelve_data,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: description(),
